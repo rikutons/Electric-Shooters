92ca2e586a46a07402f6a941a1d11e7945bf3632
rikutons <rikutonsicloud@gmail.com>
2019-09-03 13:38:30 +0900

    [ADD] BulletShooter and DamagertoEnemy

---
+++ Assets/Scripts/weapons/BulletShooter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletShooter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bulletPrefab;
+    [SerializeField]
+    private float power;
+    [SerializeField]
+    private float bulletSpeed;
+    [SerializeField]
+    private float bulletIntervalSecond;
+
+    private float timeElapsed = 0;
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (Input.GetButton("Fire1") && timeElapsed >= bulletIntervalSecond)
+        {
+            ShootBullet();
+            timeElapsed = 0;
+        }
+    }
+
+    private void ShootBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab);
+        DamagertoEnemy damagertoEnemy = bullet.GetComponent<DamagertoEnemy>();
+        damagertoEnemy.damage = power;
+        Rigidbody rigidbody = bullet.GetComponent<Rigidbody>();
+        rigidbody.AddForce(transform.up * bulletSpeed);
+    }
+}
