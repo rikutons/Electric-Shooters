425ddf9677a3527e3f80fa999f32503f9a1f86ee
rikutons <rikutonsicloud@gmail.com>
2019-09-05 15:32:05 +0900

    [ADD] reticle_assault.png

--- Assets/Scripts/Utility/PlayerRotater.cs
+++ Assets/Scripts/Utility/PlayerRotater.cs
@@ -7,11 +7,16 @@ public class PlayerRotater : MonoBehaviour
     [SerializeField]
     private GameObject yController;
 
+    [SerializeField]
+    private float ySensitivity;
+
+    [SerializeField]
+    private float xSensitivity;
     void Update()
     {
         float xRotation = Input.GetAxis("RStick X");
         float yRotation = Input.GetAxis("RStick Y");
-        transform.Rotate(0, -xRotation, 0);
-        yController.transform.transform.Rotate(yRotation, 0, 0);
+        transform.Rotate(0, -xRotation * xSensitivity, 0);
+        yController.transform.transform.Rotate(-yRotation * ySensitivity, 0, 0);
     }
 }
