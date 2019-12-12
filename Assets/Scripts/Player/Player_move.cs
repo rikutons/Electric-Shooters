b98e03265163c0c71899756cd215addf6e62c4d8
kawase00 <sumikkogurasikuma@gmail.com>
2019-12-12 18:14:08 +0900

    途中2019/12/12

--- Assets/Scripts/Player/Player_move.cs
+++ Assets/Scripts/Player/Player_move.cs
@@ -7,9 +7,9 @@ public class Player_move : MonoBehaviour
     public float speed = 3f, upspeed = 3f;
     float moveX = 0f, positionY = 0f, moveZ = 0f;
     public Rigidbody rb;
-    bool inputJumpButton = false;
+    bool inputJumpButton = false, can_jump = false;
     float oldy = 0f;
-
+    bool is_fall = false;
 
     // Start is called before the first frame update
     void Start()
@@ -24,27 +24,47 @@ public class Player_move : MonoBehaviour
         moveZ = Input.GetAxis("Vertical") * speed;
     }
 
+    private void OnTriggerExit()
+    {
+        can_jump = false;
+    }
+    private void OnTriggerEnter()
+    {
+        can_jump = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //rb.velocity = Vector3.zero;
+        is_fall = true;
+    }
+
     void FixedUpdate()
     {
-
+    
         Vector3 velocity = new Vector3(moveX, rb.velocity.y, moveZ);
+        if (is_fall)
+            velocity = new Vector3(0, -3, 0);
+        is_fall = false;
         rb.velocity = transform.TransformDirection(velocity);
         inputJumpButton = false;
 
-        Debug.Log(oldy);
-        positionY = rb.position.y;
-        Debug.Log(positionY);
+        //Debug.Log(oldy);
+        //positionY = rb.position.y;
+        //Debug.Log(positionY);
 
-        if (Input.GetButtonDown("Jump") && oldy == positionY)
+        if (Input.GetButtonDown("Jump") && can_jump == true)
         {
             inputJumpButton = true;
         }
 
-        oldy = positionY;
+        //oldy = positionY;
 
         if (inputJumpButton)
         {
+            Debug.Log("Jumped!!");
             rb.AddForce(transform.up * upspeed);
+            can_jump = false;
         }
     }
 
