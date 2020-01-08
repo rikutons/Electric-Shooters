e62f7b5d3c1022b7b8cb1dc1baacecf29a6cc799
rikutons <rikutonsicloud@gmail.com>
2020-01-08 23:11:40 +0900

    [FIX] to stick a wall in jump.

--- Assets/Scripts/Player/Player_move.cs
+++ Assets/Scripts/Player/Player_move.cs
@@ -4,67 +4,54 @@ using UnityEngine;
 
 public class Player_move : MonoBehaviour
 {
-    public float speed = 3f, upspeed = 3f;
-    float moveX = 0f, positionY = 0f, moveZ = 0f;
-    public Rigidbody rb;
-    bool inputJumpButton = false, can_jump = false;
-    float oldy = 0f;
+
+    [SerializeField]
+    private float speed, upspeed;
+    [SerializeField]
+    new private Rigidbody rigidbody;
+
+    float powerX = 0f, powerZ = 0f;
+    bool canJump = false;
     bool is_fall = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        moveX = Input.GetAxis("Horizontal") * speed;
-        moveZ = Input.GetAxis("Vertical") * speed;
+        powerX = Input.GetAxis("Horizontal") * speed;
+        powerZ = Input.GetAxis("Vertical") * speed;
+    }
+
+    private void OnTriggerStay()
+    {
+        canJump = true;
     }
 
     private void OnTriggerExit()
     {
-        can_jump = false;
-    }
-    private void OnTriggerEnter()
-    {
-        can_jump = true;
+        canJump = false;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        //rb.velocity = Vector3.zero;
-        is_fall = true;
+        if (collision.contacts[0].normal.y < 0.01)
+            is_fall = true;
+
+        else
+            is_fall = false;
     }
 
     void FixedUpdate()
     {
-    
-        Vector3 velocity = new Vector3(moveX, rb.velocity.y, moveZ);
-        if (is_fall)
-            velocity = new Vector3(0, -3, 0);
-        is_fall = false;
-        rb.velocity = transform.TransformDirection(velocity);
-        inputJumpButton = false;
-
-        //Debug.Log(oldy);
-        //positionY = rb.position.y;
-        //Debug.Log(positionY);
-
-        if (Input.GetButtonDown("Jump") && can_jump == true)
+        Vector3 velocity = new Vector3(powerX, rigidbody.velocity.y, powerZ);
+        if (is_fall && !canJump)
         {
-            inputJumpButton = true;
+            velocity.x = velocity.z = 0;
         }
+        rigidbody.velocity = transform.TransformDirection(velocity);
 
-        //oldy = positionY;
-
-        if (inputJumpButton)
+        if (Input.GetButtonDown("Jump") && canJump == true)
         {
-            Debug.Log("Jumped!!");
-            rb.AddForce(transform.up * upspeed);
-            can_jump = false;
+            rigidbody.AddForce(transform.up * upspeed);
+            canJump = false;
         }
     }
 
